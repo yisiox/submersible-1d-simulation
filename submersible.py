43d7deac01f9916c1dd9f06717db7c4cc2382641
yisiox <yisiong.y22@gmail.com>
2025-06-11 15:57:29 +0800

    submersible.py: Add comment and minor format change

--- submersible.py
+++ submersible.py
@@ -6,14 +6,14 @@ class Submersible:
 
     def __init__(
         self,
-        width: float = 0.2,         # in m
-        height: float = 0.6,        # in m
-        mass: float = 12.0,         # in kg
-        drag_coeff: float = 0.3,    # dimensionless
-        init_setpoint: float = 5.0, # in m
-        init_depth: float = 0.0,    # in m
+        width: float = 0.2,          # in m
+        height: float = 0.6,         # in m
+        mass: float = 12.0,          # in kg
+        drag_coeff: float = 0.3,     # dimensionless
+        init_setpoint: float = 5.0,  # in m
+        init_depth: float = 0.0,     # in m
         thrust_limit: float = 300.0, # in N
-        tolerance: float = 0.2,     # in m, for what considers as reached goal
+        tolerance: float = 0.2,      # in m, for what considers as reached goal
         controller: PidController = PidController(0, 0, 0)
     ) -> None:
         self.width = width
@@ -35,7 +35,7 @@ class Submersible:
 
     def get_thrust(self) -> float:
         thrust = self.controller.compute_control(self.setpoint, self.depth)
-        return min(self.thrust_limit, max(0, thrust))
+        return min(self.thrust_limit, max(0, thrust)) # 0 when going up, assumption of +ve buoyancy
     
     def update_depth(
         self,
